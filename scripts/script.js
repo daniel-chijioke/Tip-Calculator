7a7f5f4f4b78baf9c020b6b738b26b6d6729f35f
Daniel Chijioke <153667528+daniel-chijioke@users.noreply.github.com>
2026-02-14 20:27:05 +0100

    Initial upload of Tip Calculator with keyboard shortcuts

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const billAmount = document.querySelector('.js-input-bill');
+const tipPercentage = document.querySelector('.js-input-percentage');
+const buttonJs = document.querySelector('.button-style');
+
+function calculateBill() {
+    const bill = Number(billAmount.value);
+    const tip = Number(tipPercentage.value);
+    const tipAmount = (bill * tip) / 100;
+    const total = bill + tipAmount;
+    totalNew = total.toFixed(2);
+    document.querySelector('.total-output').innerHTML = `$${totalNew}`;
+}
+buttonJs.addEventListener('click', () => {
+    calculateBill();
+});
+
+
+billAmount.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        tipPercentage.focus();
+    }
+});
+
+tipPercentage.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter') {
+        calculateBill();
+    }
+});
